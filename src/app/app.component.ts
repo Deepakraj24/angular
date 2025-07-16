2f8551bd13c63d3ffe79f39701f94310f48fde61
Deepak Singh <deepugajendra@gmail.com>
2025-07-16 20:49:06 +0530

    this is demo project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, effect, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular-tut';
+//   name=""
+//   displayName="";
+//   email="";
+//   getEvent(event: Event) {
+//     const name= (event.target as HTMLInputElement).value;
+//     this.name = name;
+//   }
+//   showName() {
+//     this.displayName = this.name;
+//   }
+//   setName() {
+//     this.name = "John Doe";
+
+//   }
+// getEmail(val:string) {
+//   console.log(val);
+//   this.email = val;
+
+// }
+// setEmail() {
+//   this.email = "deepak@gamil.com";
+// }
+
+// display = false;
+// x = 10;
+count = signal(0);
+x=20;
+
+constructor() {
+  effect(() => {
+    console.log("Count changed: ", this.count());
+  })
+}
+updateCount() {
+  this.count.set(this.count() + 1);
+}
 }
